d441ef20e30cf65fa7fd42b12951e73821d9b103
Alişan <alisankarababa@gmail.com>
2023-12-16 17:24:25 +0300

    spring files

---
+++ src/main/java/com/Javas17Challenge/JavaS17ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.Javas17Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaS17ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaS17ChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Javas17Challenge/JavaS17ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Javas17Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaS17ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
